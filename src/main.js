6b7d056bb27a358bdbe217e9c4f5f95812907c6d
akdavila2 <anakarina.daviladavila@gmail.com>
2021-08-17 13:37:17 -0500

    me equivoque y no cambie de rama2

--- src/main.js
+++ src/main.js
@@ -1,66 +1,34 @@
-/* eslint-disable indent */
 // Este es el punto de entrada de tu aplicacion
 
 import { authentification } from './lib/index.js';
 import { pages } from './lib/templates.js';
 
-
-const routes = {
-    '/': pages.signin,
-    '/signup': pages.signup,
-    '/poster': pages.home
-};
 //Se manda a llamar el template principal desde template.js
 const main = document.getElementById('templates');
 main.innerHTML = pages.signin.template;
-//Metodo para verificar usuario logueado 
-auth.onAuthStateChanged((user) => {
-    if (user) {
-        main.innerHTML = pages.home.template;
-        const logout = document.querySelector('#logout');
-        logout.addEventListener('click', (e) => {
-            e.preventDefault();
-            auth.signOut()
-                .then(() => {
-                    console.log("sign out")
-                }).catch((error) => { // An error happened. 
-                });
-        })
-    } else {
-        //Se crea un evento para el botón de crear cuenta
-        main.innerHTML = pages.signin.template;
-        const createAccount = document.querySelector('#signup');
-        createAccount.addEventListener('click', e => {
-            e.preventDefault();
-            main.innerHTML = pages.signup.template;
-            const signUpForm = document.querySelector('#signupForm');
-            signUpForm.addEventListener('submit', (e) => {
-                e.preventDefault();
-                let email = document.querySelector('#signupEmail').value;
-                let password = document.querySelector('#signupPassword').value;
-                let confirmationPw = document.getElementById('signupPassword2').value;
-                const signupMesseges = document.getElementById('signupMesseges');
-                if (password === confirmationPw) {
-                    authentification(email, password)
-                } else {
-                    signupMesseges.innerHTML = `Password doesn't match`;
-                }
-                console.log(password, email);
-            })
-        })
-    }
-});
+//Se crea un evento para el botón de crear cuenta
+const createAccount = document.querySelector('#signup');
+createAccount.addEventListener('click', e => {
+    main.innerHTML = pages.signup.template;
+    const signUpForm = document.querySelector('#signupForm');
+    signUpForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        let email = document.querySelector('#signupEmail').value;
+        let password = document.querySelector('#signupPassword').value;
+        let confirmationPw = document.getElementById('signupPassword2').value;
+        const signupMesseges = document.getElementById('signupMesseges');
+        if (password === confirmationPw) {
+            authentification(email, password)
+        } else {
+            signupMesseges.innerHTML = `Password doesn't match`;
+        }
+        console.log(password, email);
+    })
+})
 
 
-
-
-//Implementado SPA
-
-export const navigate = (pathname) => {
-    window.history.pushState({}, document.title, window.location.origin + pathname);
-    if (routes[pathname] === undefined) {
-        main.innerHTML = `<img class= "error" src = 'img/error404.png'></img>`;
-        return;
-    }
-    main.innerHTML = routes[pathname]();
-};
+/*signUpForm.addEventListener('submit', (e) => { 
+    e.preventDefault();
+    const email = document.querySelector('#signup-email').value;
+    const password = document.querySelector('#signup-password').value;
+*/

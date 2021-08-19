f1f11ac55953c327868050373d65130362531ae6
akdavila2 <anakarina.daviladavila@gmail.com>
2021-08-19 16:02:52 -0500

    error de rama

--- src/main.js
+++ src/main.js
@@ -2,54 +2,66 @@
 // Este es el punto de entrada de tu aplicacion
 
 import { authentification } from './lib/index.js';
-import { pages } from './lib/templates.js';
+import { login, signUp, home, profile } from './lib/templates.js';
 
 //Rutas para el SPA 
 const routes = {
-    '/': pages.signin,
-    '/signup': pages.signup,
-    '/poster': pages.home
+    '/': login,
+    '/signup': signUp,
+    '/home': home,
+    '/profile': profile
 };
 //Se manda a llamar el template principal desde template.js
 const main = document.getElementById('templates');
-main.innerHTML = pages.signin.template;
+main.innerHTML = login;
+//funcion para mostrar el home 
+const showHome = () => {
+    main.innerHTML = home;
+    const logout = document.querySelector('#logout');
+    logout.addEventListener('click', async e => {
+        e.preventDefault();
+        try {
+            await auth.signOut;
+            main.innerHTML = login;
+            console.log('signUp');
+        } catch (e) {
+            console.log(e);
+
+        }
+    });
+};
+
 //Metodo para verificar usuario logueado 
-auth.onAuthStateChanged((user) => {
+const authStateChanged = (user) => {
     if (user) {
-        main.innerHTML = pages.home.template;
-        const logout = document.querySelector('#logout');
-        logout.addEventListener('click', (e) => {
-            e.preventDefault();
-            auth.signOut()
-                .then(() => {
-                    console.log("sign out")
-                }).catch((error) => { // An error happened. 
-                });
-        })
-    } else {
-        //Se crea un evento para el botón de crear cuenta
-        main.innerHTML = pages.signin.template;
-        const createAccount = document.querySelector('#signup');
-        createAccount.addEventListener('click', e => {
-            e.preventDefault();
-            main.innerHTML = pages.signup.template;
-            const signUpForm = document.querySelector('#signupForm');
-            signUpForm.addEventListener('submit', (e) => {
-                e.preventDefault();
-                let email = document.querySelector('#signupEmail').value;
-                let password = document.querySelector('#signupPassword').value;
-                let confirmationPw = document.getElementById('signupPassword2').value;
-                const signupMesseges = document.getElementById('signupMesseges');
-                if (password === confirmationPw) {
-                    authentification(email, password)
-                } else {
-                    signupMesseges.innerHTML = `Password doesn't match`;
-                }
-                console.log(password, email);
-            })
-        })
+        showHome();
+        return;
     }
-});
+    //Se crea un evento para el botón de crear cuenta
+    main.innerHTML = login;
+    const createAccount = document.querySelector('#signup');
+    createAccount.addEventListener('click', e => {
+        e.preventDefault();
+        main.innerHTML = signUp;
+        const signUpForm = document.querySelector('#signupForm');
+        const onSubmit = (e) => {
+            e.preventDefault();
+            let email = document.querySelector('#signupEmail').value;
+            let password = document.querySelector('#signupPassword').value;
+            let confirmationPw = document.getElementById('signupPassword2').value;
+            const signupMesseges = document.getElementById('signupMesseges');
+            if (password === confirmationPw) {
+                authentification(email, password)
+            } else {
+                signupMesseges.innerHTML = `Password doesn't match`;
+            }
+            console.log(password, email);
+        }
+        signUpForm.addEventListener('submit', onSubmit);
+    })
+
+}
+auth.onAuthStateChanged(authStateChanged);
 
 
 
@@ -57,10 +69,13 @@ auth.onAuthStateChanged((user) => {
 //Implementado SPA(Aun no funciona)
 
 export const navigate = (pathname) => {
-    window.history.pushState({}, document.title, window.location.origin + pathname);
+    window.history.pushState({}, pathname, window.location.origin + pathname);
     if (routes[pathname] === undefined) {
         main.innerHTML = `<img class= "error" src = 'img/error404.png'></img>`;
         return;
     }
     main.innerHTML = routes[pathname]();
 };
+window.onpopstate = () => {
+    main.innerHTML = routes[window.location.pathname];
+}

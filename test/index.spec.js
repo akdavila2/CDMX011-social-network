96160879774b018019a04f53e31caeb720fb5eb0
akdavila2 <anakarina.daviladavila@gmail.com>
2021-08-27 11:12:19 -0500

    modificando el archivo pack jason

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,18 @@
 // importamos la funcion que vamos a testear
 import { myFunction } from '../src/lib/index';
+import { Login } from '../'
+/**
+ * @jest-enviroment jsdom
+ */
 
-describe('myFunction', () => {
-  it('debería ser una función', () => {
-    expect(typeof myFunction).toBe('function');
-  });
+describe('Coleccion de test sobre el DOM', () => {
+    test('debería ser una función', () => {
+        expect(typeof myFunction).toBe('function');
+    });
+});
+
+describe('Coleccion de test sobre el DOM', () => {
+    test('test sobre render', () => {
+        expect(typeof myFunction).toBe('function');
+    });
 });

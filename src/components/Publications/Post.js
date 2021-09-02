3b19160103d8fec81a0aa602dc48e52992dc8ce8
akdavila2 <anakarina.daviladavila@gmail.com>
2021-09-02 13:27:48 -0500

    estilos de archivo post

--- src/components/Publications/Post.js
+++ src/components/Publications/Post.js
@@ -1,41 +1,33 @@
 import { onNavigate } from "../../router/router.js"
 import { Navbar } from "../Navbar.js"
-
 export const Post = () => {
     const viewInfo = `
-    <div class="add-post">
-    <form action="" id="pblicationsForm" class="form">
+<div id="addPublication">
+    <div class= "form-Background">
+    <form action="" id="publicationsForm" class="form">
         <label for="Username">Title of the book</label>
-        <input type="text" id="Title">
-        <label for="Username">Calification</label>
-        <input type="text" id="Calification">
-        <label for="Username">Review</label>
-        <input type="text" id="Review">
-     
-        <button class"btnPoster" id="btnAddPoster" type="submit">Add Post</button>
-        </div>
+        <input class = "inputpost" type="text" id="Title">
+        <label for="rating">Rating</label>
+        <input class = "inputpost" type="number" id="rating" max=1 0>
+        <label for="review">Review</label>
+        <textarea class = "inputpost" type="text" id="Review"></textarea>
+
+        <button class="btnPost" id="btnAddPoster" type="submit"><img class="icon-post" src="../img/plusazul.png">Add Post</button>
+
     </form>
+    </div>
 </div>
-    `
-
-    const post = document.createElement('div')
-
-    post.appendChild(Navbar())
-
-
-    const contentPoster = document.createElement('div')
-    contentPoster.classList.add('containerposter')
-    contentPoster.innerHTML = viewInfo;
-
-    post.appendChild(contentPoster);
-
-    const btnAddPost = contentPoster.querySelector('#btnAddPoster');
-
+`
+    const post = document.createElement('div');
+    post.appendChild(Navbar());
+    const contentPost = document.createElement('div');
+    contentPost.classList.add('containerpost');
+    contentPost.innerHTML = viewInfo;
+    post.appendChild(contentPost);
+    const btnAddPost = contentPost.querySelector('#btnAddPoster');
     btnAddPost.addEventListener('click', (e) => {
         e.preventDefault();
         onNavigate('/home');
     });
-
-
     return post;
 }
